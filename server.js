28080f2327002b2377fb626e15826fc98e1dc37a
Aayush7843 <aayushprajapati2926@gmail.com>
2026-06-25 17:48:33 +0530

    date update

--- server.js
+++ server.js
@@ -195,12 +195,10 @@ mongoose
        START SERVER
     ========================= */
 
-    app.listen(5000, () => {
+    const PORT = process.env.PORT || 5000;
 
-      console.log(
-        "🚀 Server running on port 5000 you can see"
-      );
-
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on port ${PORT}`);
     });
 
   })
